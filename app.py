30d68581012c806e9bd3c01920b32acbdc79f6d2
Cristi <cristi.ene@safemobile.com>
2024-12-10 18:25:44 +0200

    add month page

--- app.py
+++ app.py
@@ -18,6 +18,39 @@ def get_date(date_str: str):
     except ValueError:
         return jsonify({"error": "Invalid date format. Please use YYYY-MM-DD."})
 
+@app.route("/<int:year>/<int:month>")
+def month_page(year: int, month: int):
+    today = date.today()
+    try:
+        year = int(year)
+        month = int(month)
+        if year > today.year or (year == today.year and month > today.month):
+            return jsonify({"error": "Invalid date. Please use a date in the past."})
+        
+        end_of_month = get_last_day_of_month(month, year)
+        fx_day = get_previous_working_day(end_of_month)
+        fx_rate = get_euro_rate_from_day(fx_day)
+
+        month_days = []
+        for day in range(1, end_of_month.day + 1):
+            try:
+                requested_date = date(year, month, day)
+                data = generate_data_for_date(requested_date)
+                month_days.append(data)
+            except ValueError:
+                break
+        return render_template(
+            "month.html",
+            year=year,
+            month=month,
+            end_of_month=end_of_month,
+            fx_day=fx_day,
+            fx_rate=fx_rate,
+            month_days=month_days 
+        )
+    except ValueError:
+        return jsonify({"error": "Invalid date format. Please use YYYY-MM-DD."})
+
 @app.route("/")
 def root():
     year = 2024
@@ -26,10 +59,10 @@ def root():
         dl = get_last_day_of_month(month + 1, year)
         dd = get_previous_working_day(dl)
         rate = get_euro_rate_from_day(dd)
-        result.append({"month":f"{dd:%B}" , "end_day": f"{dl:%A, %d-%m-%Y}", "fx_day":f"{dd:%A, %d-%m-%Y}", "rate": rate})
+        result.append({"month":f"{dd:%B}" , "end_day": dl, "fx_day": dd, "rate": rate})
 
     return render_template(
-        "index.html", message=f"FX rate at the end of each month in {year}", months=result
+        "index.html",  months=result, year=year
     )
 
 if __name__ == "__main__":
